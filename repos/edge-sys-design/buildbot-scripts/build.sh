1b5b1053c3c5dc7c265cbeeffab1a91357ea9f9b
Ricky Elrod <ricky@elrod.me>
2013-01-26 17:35:40 -0500

    Minor, remove old repo before recloning.

--- repos/edge-sys-design/buildbot-scripts/build.sh
+++ repos/edge-sys-design/buildbot-scripts/build.sh
@@ -2,6 +2,7 @@
 projectdir="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$projectdir"
 
+rm -rf buildbot-scripts
 git clone git://github.com/edge-sys-design/buildbot-scripts.git
 cd buildbot-scripts
 bash move-into-place.sh
